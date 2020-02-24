f7aaa3d5434906f6f163f93e5cd0b9bff46f80bb
Olivia Thetgyi <othetgyi@gmail.com>
2020-02-24 16:52:52 +0000

    Added balance limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,15 @@
 
 class Oystercard
+  MAX_BALANCE = 90
 
-attr_accessor :balance
+  attr_accessor :balance
+
   def initialize
     @balance = 0
   end
 
   def top_up(money)
-    fail 'Maximum balance is £90' if @balance >= 90
+    fail "Maximum balance is £90" if money + balance > MAX_BALANCE
     @balance += money
   end
 end
